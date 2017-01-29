a82e2af4f965bfabca40c78289a3e1e02250a43d
selhar <gregoryjgoncalves@gmail.com>
2017-01-29 20:40:14 -0300

    tidying up, changing editor

--- data-visualization/leaderboard/src/App.js
+++ data-visualization/leaderboard/src/App.js
@@ -1,19 +1,7 @@
-import React, {
-  Component
-} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Leaderboard/>
-      </div>
-    );
-  }
-}
-
-class Leaderboard_backend extends Component {
   constructor(props) {
     super();
 
@@ -26,16 +14,19 @@ class Leaderboard_backend extends Component {
   fetch_points(param) {
     let that = this;
     fetch('https://fcctop100.herokuapp.com/api/fccusers/top/' + param, {
-      method: 'get' // opcional
-    }).then(function(response) {
-      response.json().then(function(result) {
-        that.setState({
-          ["data_" + param]: result
+        method: 'get'
+      }).then(function (response) {
+      response
+        .json()
+        .then(function (result) {
+          that.setState({
+            ["data_" + param]: result
+          })
         })
-      })
-    }).catch(function(err) {
-      console.error(err);
-    });
+    })
+      .catch(function (err) {
+        console.error(err);
+      });
   }
 
   componentDidMount() {
@@ -44,31 +35,31 @@ class Leaderboard_backend extends Component {
   }
 
   render() {
-    return (
-      <Leaderboard_frontend recent={this.state.data_recent}, lifetime={this.state.data_lifetimec}/>
-    )
+    return (<Leaderboard_frontend
+      recent={this.state.data_recent}
+      lifetime={this.state.data_lifetimec}/>)
   }
 }
 
-class Leaderboard_frontend extends Component {
-  render(){
-      <div className="board">
-        <header>Freecodecamp brownie leaderboard </header>
-        <table>
-          <tr>
-            <th>#</th>
-            <th>Nickname</th>
-            <th>Last 30 days</th>
-            <th>Lifetime</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>abc</td>
-            <td>1000</td>
-            <td>1000</td>
-          </tr>
-        </table>
-      </div>
+class Leaderboard extends Component {
+  render() { 
+    <div className = "board"> 
+      <header>Freecodecamp brownie leaderboard </header> 
+      <table> 
+        <tr>
+          <th>#</th>
+          <th>Nickname</th>
+          <th>Last 30 days</th>
+          <th>Lifetime</th>
+        </tr> 
+        <tr> 
+          <td>1</td> 
+          <td>abc</td>
+            <td>1000</td > 
+            <td>1000</td> 
+        </tr>
+      </table> 
+   </div>
   }
 }
 export default App;
