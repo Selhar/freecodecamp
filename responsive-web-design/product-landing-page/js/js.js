57de95e8d49437a64a376dc979d13c3950a35708
selhar <gregoryjgoncalves@gmail.com>
2017-02-21 19:47:40 -0300

    added gallery functionality

--- responsive-web-design/product-landing-page/js/js.js
+++ responsive-web-design/product-landing-page/js/js.js
@@ -1,15 +1,43 @@
-let gallery = document.getElementById("gallery");
-let gallery_items = gallery.children[0].children[0].children;
+ready(() => {
+    mouseover_gallery();
+});
 
-gallery.addEventListener("mouseover", (event) => {
+function mouseover_gallery() {
+    let gallery = document.getElementById("gallery");
+    let gallery_items = gallery.children[0].children[0].children;
 
-    if (event.target instanceof HTMLImageElement) {
-        for (let i = 0; i < gallery_items.length; i++) {
-            if (gallery_items[i] === event.target) {
-                event.target.className += " img-selected";
-            } else {
-                gallery_items[i].className = "img-selection";
+    gallery.addEventListener("mouseover", (event) => {
+        let selected_thumbnail = event.target;
+
+        if (selected_thumbnail instanceof HTMLImageElement) {
+            for (let i = 0; i < gallery_items.length; i++) {
+                if (gallery_items[i] === selected_thumbnail) {
+                    selected_thumbnail.className += " img-selected";
+                    let new_background = retrieve_style(selected_thumbnail, "background-image");
+                    change_background(new_background);
+                } else {
+                    gallery_items[i].className = "img-selection";
+                }
             }
         }
+    });
+}
+
+function change_background(new_background) {
+    current_background = document.getElementById("parallax");
+    current_background.style.backgroundImage = new_background;
+}
+
+function retrieve_style(element, property) {
+    style = window.getComputedStyle(element);
+    return style.getPropertyValue(property);
+}
+
+//Function from http://youmightnotneedjquery.com/, brety gud
+function ready(fn) {
+    if (document.readyState != 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
     }
-});
+}
