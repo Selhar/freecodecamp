5362ba1d7d9c759190fcec4d1dc3995bb1188b36
selhar <gregoryjgoncalves@gmail.com>
2017-02-21 00:55:55 -0300

    finished selection

--- responsive-web-design/product-landing-page/js/js.js
+++ responsive-web-design/product-landing-page/js/js.js
@@ -1,14 +1,15 @@
 let gallery = document.getElementById("gallery");
+let gallery_items = gallery.children[0].children[0].children;
 
 gallery.addEventListener("mouseover", (event) => {
 
     if (event.target instanceof HTMLImageElement) {
-        clearClasses(event.target);
-        event.target.className += " img-selected";
+        for (let i = 0; i < gallery_items.length; i++) {
+            if (gallery_items[i] === event.target) {
+                event.target.className += " img-selected";
+            } else {
+                gallery_items[i].className = "img-selection";
+            }
+        }
     }
 });
-
-function clearClasses(target) {
-
-    console.log(target);
-}
