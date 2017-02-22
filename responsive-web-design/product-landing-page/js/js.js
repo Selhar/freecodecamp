9b4fbcbea76c3760ed8c4dd9bef6a38222289211
selhar <gregoryjgoncalves@gmail.com>
2017-02-21 21:27:49 -0300

    finished gallery bitch, images need croppin tho

--- responsive-web-design/product-landing-page/js/js.js
+++ responsive-web-design/product-landing-page/js/js.js
@@ -1,24 +1,25 @@
 ready(() => {
     mouseover_gallery();
 });
+let images_enum = {
+    /* This enum links a thumbnail to the full-sized image and vice-versa.
+        This is only hard-coded because i'm not uploading locally
+        this project will be uploaded to codepen, thus i have to upload these images somewhere
+        and since i can't predict how the links will be, i must hardcode the relation between thumb e fullscreen image.
+        In a server, this should be done in a more dynamic way, using local system's filepath instead of hardcoded enum*/
+    'url("http://res.cloudinary.com/selhar/image/upload/v1487721126/cupcake1-thumb_cofbn6.jpg")': 'url("http://res.cloudinary.com/selhar/image/upload/v1487721137/cupcake1_bv8sxl.jpg")',
+    'url("http://res.cloudinary.com/selhar/image/upload/v1487721125/cupcake2-thumb_x0xee4.jpg")': 'url("http://res.cloudinary.com/selhar/image/upload/v1487721156/cupcake2_nlanvv.jpg")',
+    'url("http://res.cloudinary.com/selhar/image/upload/v1487721126/cupcake3-thumb_qdnfs3.jpg")': 'url("http://res.cloudinary.com/selhar/image/upload/v1487721168/cupcake3_uhwgyz.jpg")',
+    'url("http://res.cloudinary.com/selhar/image/upload/v1487721127/cupcake4-thumb_bjf5xq.jpg")': 'url("http://res.cloudinary.com/selhar/image/upload/v1487721153/cupcake4_gwkhrq.jpg")',
+    'url("http://res.cloudinary.com/selhar/image/upload/v1487721130/cupcake5-thumb_adtsiq.jpg")': 'url("http://res.cloudinary.com/selhar/image/upload/v1487721137/cupcake5_i78odb.jpg")',
+    'url("http://res.cloudinary.com/selhar/image/upload/v1487721142/cupcake6-thumb_t2uow1.jpg")': 'url("http://res.cloudinary.com/selhar/image/upload/v1487721168/cupcake6_rebcw5.jpg")',
+}
 
 function mouseover_gallery() {
-    let images_enum = {
-        /* This enum links a thumbnail to the full-sized image and vice-versa.
-            This is only hard-coded because i'm not uploading locally
-            this project will be uploaded to codepen, thus i have to upload these images somewhere
-            and since i can't predict how the links will be, i must hardcode the relation between thumb e fullscreen image.
-            In a server, this should be done in a more dynamic way, using local system's filepath instead of hardcoded enum*/
-        "url('http://res.cloudinary.com/selhar/image/upload/v1487718770/cupcake6-thumb_r9very.jpg')": "url('http://res.cloudinary.com/selhar/image/upload/v1487718821/cupcake6_eetqx2.jpg')",
-        "url('http://res.cloudinary.com/selhar/image/upload/v1487718775/cupcake5-thumb_abke1a.jpg')": "url('http://res.cloudinary.com/selhar/image/upload/v1487718851/cupcake5_qk8voq.jpg')",
-        "url('http://res.cloudinary.com/selhar/image/upload/v1487718767/cupcake4-thumb_y0x509.jpg')": "url('http://res.cloudinary.com/selhar/image/upload/v1487718838/cupcake4_crn1hn.jpg')",
-        "url('http://res.cloudinary.com/selhar/image/upload/v1487718767/cupcake3-thumb_x6wh3p.jpg')": "url('http://res.cloudinary.com/selhar/image/upload/v1487718823/cupcake3_uzzxo9.jpg')",
-        "url('http://res.cloudinary.com/selhar/image/upload/v1487718776/cupcake2-thumb_xnbtho.jpg')": "url('http://res.cloudinary.com/selhar/image/upload/v1487718856/cupcake2_ndnzzd.jpg')",
-        "url('http://res.cloudinary.com/selhar/image/upload/v1487718762/cupcake1-thumb_lae7pl.jpg')": "url('http://res.cloudinary.com/selhar/image/upload/v1487718846/cupcake1_nfy97a.jpg')"
-    }
+
 
     let gallery = document.getElementById("gallery");
-    let gallery_items = gallery.children[0].children[0].children;
+    let gallery_items = gallery.children[1].children[0].children;
 
     gallery.addEventListener("mouseover", (event) => {
         let selected_thumbnail = event.target;
@@ -28,6 +29,7 @@ function mouseover_gallery() {
                 if (gallery_items[i] === selected_thumbnail) {
                     selected_thumbnail.className += " img-selected";
                     let new_background = retrieve_style(selected_thumbnail, "background-image");
+                    console.log(new_background);
                     change_background(new_background);
                 } else {
                     gallery_items[i].className = "img-selection";
@@ -43,9 +45,8 @@ function change_background(new_background) {
 }
 
 function retrieve_style(element, property) {
-    style = window.getComputedStyle(element);
-    console.log(style.getPropertyValue(property));
-    return style.getPropertyValue(property);
+    style = window.getComputedStyle(element).getPropertyValue(property);
+    return images_enum[style];
 }
 
 //Function from http://youmightnotneedjquery.com/, brety gud
