d8b31f17d2b04a7a0da9077556f31e5d57472551
selhar <gregoryjgoncalves@gmail.com>
2017-02-24 04:37:09 -0300

    finished

--- responsive-web-design/product-landing-page/js/js.js
+++ responsive-web-design/product-landing-page/js/js.js
@@ -2,10 +2,12 @@ ready(() => {
     browse_gallery();
 });
 let images_enum = {
-    /* This enum solution is only necessary because i'll upload this project to codepen
-        thus, i must upload each image to a third party server, so i can't predict how the URL is gonna be.
-        Since i must have a relation between thumbnail and full image, and their links are unpredictable, i hard coded enums.
-        Ideally this would use the filepath instead of enums, that way you don't rely on hardcoded links, only generic references.*/
+    /*  This enum solution is only necessary because i'll upload this project to codepen
+        each image has to be uploaded to a third party server and since i can't predict 
+        how each URL is going to be like, i had to hardcode it in after uploading each image.
+        For the gallery to work i need a relation between the button thumbnail and the fullscreen image
+        so i used enums. In a server the filepath should be used, instead of hardcoded path-to-file.
+        .*/
     'url("http://res.cloudinary.com/selhar/image/upload/v1487737607/cupcake1-thumb_hfwyi0.jpg")': 'url("https://res.cloudinary.com/selhar/image/upload/v1487737627/cupcake1_k0bvig.jpg")',
     'url("http://res.cloudinary.com/selhar/image/upload/v1487737608/cupcake2-thumb_vlza22.jpg")': 'url("https://res.cloudinary.com/selhar/image/upload/v1487737639/cupcake2_eu8jv5.jpg")',
     'url("http://res.cloudinary.com/selhar/image/upload/v1487910173/cupcake3-thumb_ctqzwy.jpg")': 'url("http://res.cloudinary.com/selhar/image/upload/v1487910173/cupcake3_fd11r6.jpg")',
@@ -19,11 +21,12 @@ function browse_gallery() {
     let gallery = document.getElementById("gallery-menu");
     let gallery_items = gallery.children;
 
-
     gallery.addEventListener("mouseover", (event) => {
         let selected_thumbnail = event.target;
         change_image(selected_thumbnail, gallery, gallery_items);
     });
+
+    //Click event for mobile users
     gallery.addEventListener("click", (event) => {
         let selected_thumbnail = event.target;
         change_image(selected_thumbnail, gallery, gallery_items);
@@ -34,7 +37,7 @@ function change_image(selected_thumbnail, gallery, gallery_items) {
     if (selected_thumbnail instanceof HTMLImageElement) {
         for (let i = 0; i < gallery_items.length; i++) {
             if (gallery_items[i] === selected_thumbnail) {
-                selected_thumbnail.className += " img-selected";
+                selected_thumbnail.className += " img-selected animated headShake";
                 let new_background = retrieve_style(selected_thumbnail, "background-image");
                 change_background(new_background);
             } else {
