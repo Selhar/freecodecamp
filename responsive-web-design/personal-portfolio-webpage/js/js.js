56fabdd2c9cd3b5f562ba9440f01492bbd8c2d0b
selhar <gregoryjgoncalves@gmail.com>
2017-03-02 09:17:36 -0300

    animation procedure complete, lacks common sense and good taste

--- responsive-web-design/personal-portfolio-webpage/js/js.js
+++ responsive-web-design/personal-portfolio-webpage/js/js.js
@@ -1,13 +1,19 @@
 ready(() => {
-    const FRONT_END = ["html5", "css3", "react.js", "gsap", "bulma", "sass", "javascript"];
+    const FRONT_END = ["html5", "css3", "react", "redux", "gsap", "bulma", "sass", "javascript"];
     const DATA_VISUALIZATION = ["D3.js"];
     const BACK_END = ["node.js", "express", "mongoDB", "mongoose", "postgres"];
-    const SECURITY = ["safe code", "helmet.js", "Chai", "pentesting"];
+    const SECURITY = ["helmet.js", "Chai"];
     const data = {
         "frontEnd-techs": FRONT_END,
         "dataVisualization-techs": DATA_VISUALIZATION,
         "backEnd-techs": BACK_END,
         "security-techs": SECURITY
+    }
+
+    let title_lines = document.getElementsByClassName("title-line");
+
+    for (let line of title_lines) {
+        TweenLite.to(line, 4, { width: "100%" });
     }
 
     inject_menu_data(data);
@@ -20,11 +26,11 @@ ready(() => {
 
         title.addEventListener("mouseover", () => {
             console.log(techs);
-            TweenLite.fromTo(techs, 1, { top: 200, color: "rgba(0, 0, 0, 0)" }, { top: 0, color: "rgba(0, 0, 0, 1)" });
+            TweenLite.fromTo(techs, 0.5, { top: 200, color: "rgba(0, 0, 0, 0)" }, { top: 0, color: "rgba(0, 0, 0, 1)" });
         });
         title.addEventListener("mouseout", () => {
             console.log(techs);
-            TweenLite.fromTo(techs, 1, { bottom: 0, color: "rgba(0, 0, 0, 1)" }, { top: 200, color: "rgba(0, 0, 0, 0)" });
+            TweenLite.fromTo(techs, 0.5, { bottom: 0, color: "rgba(0, 0, 0, 1)" }, { top: 200, color: "rgba(0, 0, 0, 0)" });
         });
     }
 });
