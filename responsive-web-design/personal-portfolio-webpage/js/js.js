dc9eaedd705b9fc738f02d4736090a84f93576e7
selhar <gregoryjgoncalves@gmail.com>
2017-03-02 04:17:27 -0300

    items structure done

--- responsive-web-design/personal-portfolio-webpage/js/js.js
+++ responsive-web-design/personal-portfolio-webpage/js/js.js
@@ -1,7 +1,29 @@
 ready(() => {
+    const FRONT_END = ["react.js", "gsap", "bulma", "sass", "javascript"];
+    const DATA_VISUALIZATION = ["D3.js"];
+    const BACK_END = ["node.js", "express", "mongoDB", "mongoose", "postgres"];
+    const SECURITY = ["safe code", "helmet.js", "Chai", "pentesting"];
+    const data = {
+        "frontEnd": FRONT_END,
+        "dataVisualization": DATA_VISUALIZATION,
+        "backEnd": BACK_END,
+        "security": SECURITY
+    }
 
+    inject_menu_data(data);
 });
 
+function inject_menu_data(data) {
+    for (let id of Object.keys(data)) {
+        let item = document.getElementById(id);
+
+        item.innerHTML = data[id].map((value) => {
+            return "<li>" + value + "</li>";
+        }).toString().replace(/,/g, "");
+
+
+    }
+}
 
 function ready(fn) {
     if (document.readyState != 'loading') {
