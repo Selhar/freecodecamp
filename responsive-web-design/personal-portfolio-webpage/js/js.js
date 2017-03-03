e89383f43fa734420cc9b883e0d0df053062b50a
selhar <gregoryjgoncalves@gmail.com>
2017-03-03 00:19:23 -0300

    fuck it, redoing from scratch, fuck it, fuck it, fucking fuck it

--- responsive-web-design/personal-portfolio-webpage/js/js.js
+++ responsive-web-design/personal-portfolio-webpage/js/js.js
@@ -1,10 +1,6 @@
 ready(() => {
 
-    let title_lines = document.getElementsByClassName("title-line");
 
-    for (let line of title_lines) {
-        TweenLite.to(line, 4, { width: "100%" });
-    }
 });
 
 function get_css_property(element, property) {
