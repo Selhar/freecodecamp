3a482fb6ea8bd6463c7429695be49fed40099e3c
selhar <gregoryjgoncalves@gmail.com>
2017-03-03 06:02:05 -0300

    it's still shit, im done tho

--- responsive-web-design/personal-portfolio-webpage/js/js.js
+++ responsive-web-design/personal-portfolio-webpage/js/js.js
@@ -1,12 +1,24 @@
 ready(() => {
 
-    let title_lines = document.getElementsByClassName("title-lines");
+    animate_arrow();
+    animate_title();
 
+});
+
+function animate_title() {
+    let title_lines = document.getElementsByClassName("title-lines");
     for (let line of title_lines) {
         TweenLite.to(line, 5, { width: "100%" });
     }
+}
 
-});
+function animate_arrow() {
+    let timeline = new TimelineMax({ repeat: -1 });
+    let arrow = document.getElementById("arrow-button");
+
+    timeline.add(TweenLite.to(arrow, 0.5, { bottom: "1rem" }));
+    timeline.add(TweenLite.to(arrow, 0.5, { bottom: "2rem" }));
+}
 
 function get_css_property(element, property) {
     let sheet = window.getComputedStyle(element);
