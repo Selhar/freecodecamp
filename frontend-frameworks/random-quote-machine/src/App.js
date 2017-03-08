3e1d7f9e6c498a349642d81c8e9cf79e3170f035
selhar <gregoryjgoncalves@gmail.com>
2017-03-08 01:36:14 -0300

    bulma working

--- frontend-frameworks/random-quote-machine/src/App.js
+++ frontend-frameworks/random-quote-machine/src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import '/style/css/App.css';
+import './style/css/app.css';
 
 class App extends Component {
     render() {
         return (
-            <div className="board"></div>
+            <div className="board columns column is-6 is-offset-3"></div>
         );
     }
 }
