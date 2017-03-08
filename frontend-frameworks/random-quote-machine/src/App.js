ffd7c0aa7c2a5938dac47a95723de9283940a058
selhar <gregoryjgoncalves@gmail.com>
2017-03-08 07:25:51 -0300

    solved all configuration problems, started the basic layout

--- frontend-frameworks/random-quote-machine/src/App.js
+++ frontend-frameworks/random-quote-machine/src/App.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
 import './style/css/app.css';
 
+class TextField extends Component {
+    render(){
+        return (
+            <div></div>
+        )
+    }
+}
+
+class Footer extends Component{
+    render(){
+        return (
+            <footer className="footer-quote">
+                <a className="button">New quote</a>
+                <a className="button">Tweet</a>
+            </footer>
+        )
+    }
+}
 class App extends Component {
     render() {
         return (
-            <div className="board columns column is-6 is-offset-3"></div>
+            <div className="board">
+                <h3 className="title is-3">Quote generator</h3>
+                <TextField/>
+                <Footer/>
+            </div>
         );
     }
 }
