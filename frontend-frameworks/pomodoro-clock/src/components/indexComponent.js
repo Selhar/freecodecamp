b70c71e9f2626a3f4dc61f5df7f1bf65a9ef08cf
selhar <gregoryjgoncalves@gmail.com>
2017-03-15 05:16:23 -0300

    finished the css

--- frontend-frameworks/pomodoro-clock/src/components/indexComponent.js
+++ frontend-frameworks/pomodoro-clock/src/components/indexComponent.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 export default () => (
-    <div>
+    <div className="clock">
+        <input type="text" defaultValue="25:00"/>
+        <a className="button">RESET</a>
+        <a className="button">START</a> 
     </div>
 )
