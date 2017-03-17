604a177b83b8bb4c4fb736120c394449698c1ca6
selhar <gregoryjgoncalves@gmail.com>
2017-03-17 07:00:20 -0300

    finished input and output functionality

---
+++ frontend-frameworks/markup-visualization/src/components/processedInput.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {updateInput} from '../actions/indexAction';
+import {bindActionCreators} from 'redux';
+import marked from 'marked';
+
+class processedInput extends Component{
+    render(){
+        const {text} = this.props;
+        const createMarkup = {
+            __html: marked(text)
+        }
+        return(
+            <div dangerouslySetInnerHTML={createMarkup} disabled></ div>
+    )}
+}
+
+export default connect(
+   state => ({
+       text: state.text
+    })
+)(processedInput);
