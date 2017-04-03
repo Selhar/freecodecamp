0dcab3e753df7347221645916110da6fea95b40b
Selhar <selhar@protonmail.com>
2017-04-03 14:12:49 -0300

    testing with waterfall

--- apis-and-microservices/url-shortener-microservice/controllers/add_url.js
+++ apis-and-microservices/url-shortener-microservice/controllers/add_url.js
@@ -1,18 +1,33 @@
 const UrlModel = require('../models/url');
 const http_request = require('request');
-const url = require("url");
+const url = require('url');
+const waterfall = require("async/waterfall");
 
 exports.addUrl = (request, response) => {
-    const full_url = request.body.url;
-    
-    http_request(full_url, (error, reply) => {
-        if(!error && reply.statusCode === 200){
-            const sanitized_url = full_url.replace(/(^\w+:|^)\/\/(w{3}.)?/, '');
-            return response.json({sanitized: sanitized_url});
-        }else{
-            return response.json({error: "The provided URL is either invalid or currently offline."});
-        }
-    });
+    const full_url = request.body.url;    
+waterfall([
+    (callback) => {
+        http_request(full_url, (error, result) => {
+            if(error){
+                callback(error);
+                return;
+            }
+            if(result.statusCode === 200){
+                const sanitized_url = full_url.replace(/(^\w+:|^)\/\/(w{3}.)?/, '');
+                callback(null, sanitized_url);
+            }
+        });
+    },(sanitized_url, callback) => {
+        response.json({boga: sanitized_url});
+        callback(null);
+    }
+], (error, result) => {
+    if(error){
+        response.json({error: "An unidentified error ocurred."});
+        console.log(error);
+    }
+})
+   
 };
 
 function findShortUrl(url){
@@ -28,6 +43,33 @@ function findShortUrl(url){
 
 
 /*
+ http_request(full_url, (error, reply) => {
+        //Validate URL
+        if(!error && reply.statusCode === 200){
+            const sanitized_url = full_url.replace(/(^\w+:|^)\/\/(w{3}.)?/, '');
+            //Check for the sanitized URL in DB
+            UrlModel.findOne({original_url: sanitized_url}, (queryError, queryResult) => {
+                if(!queryError && queryResult){
+                    return response.json({original_url: queryResult.original_url, short_url: queryResult.short_url});
+                }else{
+                    let isShortUrlValid = false;
+                    let short_url = sanitized_url.slice(0, 4);
+                    let index = 0;
+                    //Process data until i find a short URL that is not present in the DB
+                    while(!isShortUrlValid){
+                        UrlModel.findOne({short_url: short_url}, (smallUrlerror, smallUrlResult) => {
+                            return true;
+                        });
+                    }
+                }
+            })
+        }else{
+            return response.json({error: "The provided URL is either invalid or currently offline."});
+        }
+    });
+
+
+
             if(error){
                 output_error(error);
                 return response.json({error: "An udentified error ocurred."});
