689a29f9bebc1e481bba08e36921cbce03fd01bc
Selhar <selhar@protonmail.com>
2017-04-04 15:50:37 -0300

    reestructuring

--- apis-and-microservices/url-shortener-microservice/controllers/add_url.js
+++ apis-and-microservices/url-shortener-microservice/controllers/add_url.js
@@ -5,46 +5,50 @@ const waterfall = require("async/waterfall");
 
 exports.addUrl = (request, response) => {
     const full_url = request.body.url;    
-    waterfall([ function(callback) {
-        http_request(full_url, (error, result) => {
-                if(error){
-                    return callback(error);
-                }
-                if(result.statusCode === 200){
-                    const sanitized_url = full_url.replace(/(^\w+:|^)\/\/(w{3}.)?/, '');
-                    return callback(null, sanitized_url);
-                }
-            });
-        },function isUrlInDatabase(sanitized_url, callback) {
+    
+    waterfall([ 
+        function(callback) {
+            http_request(full_url, (error, result) => {
+                    if(error){
+                        return callback(error);
+                    }
+                    if(result.statusCode === 200){
+                        const sanitized_url = full_url.replace(/(^\w+:|^)\/\/(w{3}.)?/, '');
+                        return callback(null, {full_url: sanitized_url});
+                    }
+                });
+        },function isUrlInDatabase(data, callback) {
 
-            UrlModel.findOne({original_url: sanitized_url}, (error, result) => {
+            UrlModel.findOne({original_url: data.sanitized_url}, (error, result) => {
                 if(error){
                     return callback(error);
                 } else if(result){
                     return done(null, {original_url: result.original_url, short_url: result.short_url}, response);
                 } else{
-                    return callback(null, sanitized_url);
+                    return callback(null, data.sanitized_url);
                 }
             });
-        },function processShortUrl(sanitized_url, callback) {
+        },function processShortUrl(data, callback) {
                 
-            let isShortUrlUnique = false;
-            let short_url = sanitized_url.slice(0, 4);
-            let index = 0;
+            let isShortUrlUnique = data.unique || false;
+            let short_url = data.sanitized_url || data.sanitized_url.slice(0, 3);
+            let index = data.index || 0;
             
-            while(!isShortUrlUnique){
-                UrlModel.findOne({short_url: short_url}, (error, result) => {
-                    if(error){
-                        return callback(error);
-                    }
+            UrlModel.findOne({short_url: short_url}, (error, result) => {
+                if(error){
+                    return callback(error);
+                } 
+                if(result){    
                     if(short_url.length > 3){
-                        short_url.replace(index, ++index);
+                        short_url = short_url.replace(index, ++index);
                     }else{
                         short_url += index++;
                     }
-                });            
+                }else{
+                    isShortUrlUnique = true;
+                }   
+            });
                 return callback(null, short_url, sanitized_url);
-            }
         },function saveToDatabase(short_url, sanitized_url, callback) {
             
             let new_url = new UrlModel({
@@ -61,13 +65,18 @@ exports.addUrl = (request, response) => {
         }
     ],done);
 
-}
+    function done(error, result) {
+        if(error){
+            switch(error.code){
+            case 'ENOTFOUND':
+                return response.json({error: "the url provided could not be found."});
+            default:
+                console.log(error);
+                return response.json({error: "an unidentified error ocurred."});
+            }
+        }
 
-function done(error, result, response) {
-    if(error){
-        console.log(error);
-        return response.json({error: "An unidentified error ocurred."});
+        return response.json(result);
     }
-    return response.json(result);
+
 }
-   
--- apis-and-microservices/url-shortener-microservice/models/url.js
+++ apis-and-microservices/url-shortener-microservice/models/url.js
@@ -3,7 +3,7 @@ const schema = mongoose.Schema;
 
 const url_schema = new schema ({
   original_url: {type: String, required: true, unique: true},
-  short_url: {type: String, required: true}
+  short_url: {type: String, required: true, unique: true}
 });
 
 module.exports = mongoose.model('UrlSchema', url_schema);
