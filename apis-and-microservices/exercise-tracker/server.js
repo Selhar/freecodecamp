059a14619551230975c2a062f29c46e35318e250
Selhar <selhar@protonmail.com>
2017-04-06 22:17:30 -0300

    sick today, daily commit tho

--- apis-and-microservices/exercise-tracker/server.js
+++ apis-and-microservices/exercise-tracker/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const server = express();
 const root = process.cwd();
+const controller = require('./controllers/index');
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
 server.use('/public', express.static(root + '/public'));
 
 server.get('/', (request, response) => {
