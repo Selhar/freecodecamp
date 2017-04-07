d276c585abbb8336aabb7613a5352eacac77cd34
Selhar <selhar@protonmail.com>
2017-04-07 14:02:03 -0300

    boilerplate

--- apis-and-microservices/exercise-tracker/models/exercise.js
+++ apis-and-microservices/exercise-tracker/models/exercise.js
@@ -5,7 +5,7 @@ const Exercises = new Schema({
   description: { type: String, required: true },
   duration: { type: Number, required: true},
   date: { type: Date, default: Date.now },
-  _user: { type: number, ref: 'User' }
+  _user: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 Exercises.pre('save', function(next) {
