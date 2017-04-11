77a9e66d57161f12a7cd680fdd9eb65f21cdf0d9
Selhar <selhar@protonmail.com>
2017-04-10 21:23:27 -0300

    finished

--- apis-and-microservices/file-metadata/server.js
+++ apis-and-microservices/file-metadata/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const server = express();
 const root = process.cwd();
+const multer = require('multer');
+const upload = multer({ dest: './storage' });
+
+
 server.use('/public', express.static(root + '/public'));
 
 server.get('/', (request, response) => {
     response.render(root + '/views/index.ejs');
+});
+
+server.post('/api/fileanalyse',upload.single('upfile'), function(req, res){
+   res.json({
+    'name' : req.file.originalname,
+    'type' : req.file.mimetype,
+    'size' : req.file.size
+   });
 });
 
 server.get('*', (request, response) => {
