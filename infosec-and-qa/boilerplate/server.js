17fb75fee6a8f0dcb24af996bacc76b4c8cd7baa
Selhar <selhar@protonmail.com>
2017-04-13 10:52:04 -0300

    setting up infosec and qa boilerplate

---
+++ infosec-and-qa/boilerplate/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const server = express();
+const root = process.cwd();
+server.use('/public', express.static(root + '/public'));
+
+server.get('/', (request, response) => {
+    response.render(root + '/views/index.ejs');
+});
+
+server.get('*', (request, response) => {
+    response.send('<p>Bad, bad user. No donuts for you.</p>');
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Listening");
+});
