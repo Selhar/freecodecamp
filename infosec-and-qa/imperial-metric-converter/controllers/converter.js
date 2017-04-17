bc517d3c238120ec7ad7cfbf7dccd29865ecdf8d
Selhar <selhar@protonmail.com>
2017-04-17 06:13:38 -0300

    initial tests

---
+++ infosec-and-qa/imperial-metric-converter/controllers/converter.js
@@ -0,0 +1,5 @@
+exports.converter = (value) => {
+    const galToL = 3.78541;
+    const lbsToKg = 0.453592;
+    const miToKm = 1.60934;
+}
--- infosec-and-qa/imperial-metric-converter/tests/unit_tests.js
+++ infosec-and-qa/imperial-metric-converter/tests/unit_tests.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const assert = chai.assert;
+const converter = require('../controllers/index');
 
 suite('Unit testing', () => {
   test('No Numerical Input', function(done) {
