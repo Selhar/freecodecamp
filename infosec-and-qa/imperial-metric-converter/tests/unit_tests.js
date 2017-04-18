b8558e895b67c5d7dda9ddda1281f506d8e6eabf
Selhar <selhar@protonmail.com>
2017-04-17 22:05:42 -0300

    unit tests

--- infosec-and-qa/imperial-metric-converter/tests/unit_tests.js
+++ infosec-and-qa/imperial-metric-converter/tests/unit_tests.js
@@ -8,15 +8,17 @@ suite('Unit testing', () => {
   suite('Input processing', () => {
     test('Typeof', (done) => {
       assert.typeOf(converter.processInput('1gal'), 'object');
+      assert.typeOf(converter.processInput('1gal').value, 'number');
+      assert.typeOf(converter.processInput('1gal').type, 'string');
       done();
     });
     test('Object returned', function(done) {
-      assert.equal(converter.processInput('1gal'),{value: 1, type: "gal"});
-      assert.equal(converter.processInput('1lbs'),{value: 1, type: "lbs"});
-      assert.equal(converter.processInput('1mi'),{value: 1, type: "mi"});
-      assert.equal(converter.processInput('1l'),{value: 1, type: "l"});
-      assert.equal(converter.processInput('1kg'),{value: 1, type: "kg"});
-      assert.equal(converter.processInput('1km'),{value: 1, type: "km"});
+      assert.deepEqual(converter.processInput('1gal'), {value: 1, type: 'gal'});
+      assert.deepEqual(converter.processInput('1lbs'), {value: 1, type: 'lbs'});
+      assert.deepEqual(converter.processInput('1mi'),  {value: 1, type: 'mi'});
+      assert.deepEqual(converter.processInput('1l'),   {value: 1, type: 'l'});
+      assert.deepEqual(converter.processInput('1kg'),  {value: 1, type: 'kg'});
+      assert.deepEqual(converter.processInput('1km'),  {value: 1, type: 'km'});
       done();
     }); 
   });
