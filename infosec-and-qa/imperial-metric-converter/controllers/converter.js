ba23be336b606525305464e883a531c31dfaa6f3
Selhar <selhar@protonmail.com>
2017-04-17 21:50:54 -0300

    unit tests

--- infosec-and-qa/imperial-metric-converter/controllers/converter.js
+++ infosec-and-qa/imperial-metric-converter/controllers/converter.js
@@ -4,12 +4,11 @@ function converter (){
     const miToKm = 1.60934;
     
     this.processInput = (input) => {
-        const value = input.replace(/\D/g,'');
-        const type = input.find(/\D/g);
+        const value = Number(input.replace(/\D/g,''));
+        const type = input.match(/\D/g).join('');
 
         return {value: value, type: type};
     }
 }
-
 
 module.exports = converter;
--- infosec-and-qa/imperial-metric-converter/tests/unit_tests.js
+++ infosec-and-qa/imperial-metric-converter/tests/unit_tests.js
@@ -5,14 +5,19 @@ let converter = require('../controllers/converter');
 converter = new converter();
 
 suite('Unit testing', () => {
-  test('Input processing', function(done) {
-    assert.equal(converter.processInput('1gal'),{value: 1, type: "gal"});
-    assert.equal(converter.processInput('1lbs'),{value: 1, type: "lbs"});
-    assert.equal(converter.processInput('1mi'),{value: 1, type: "mi"});
-    assert.equal(converter.processInput('1l'),{value: 1, type: "l"});
-    assert.equal(converter.processInput('1kg'),{value: 1, type: "kg"});
-    assert.equal(converter.processInput('1km'),{value: 1, type: "km"});
-    done();
-  }); 
-  
+  suite('Input processing', () => {
+    test('Typeof', (done) => {
+      assert.typeOf(converter.processInput('1gal'), 'object');
+      done();
+    });
+    test('Object returned', function(done) {
+      assert.equal(converter.processInput('1gal'),{value: 1, type: "gal"});
+      assert.equal(converter.processInput('1lbs'),{value: 1, type: "lbs"});
+      assert.equal(converter.processInput('1mi'),{value: 1, type: "mi"});
+      assert.equal(converter.processInput('1l'),{value: 1, type: "l"});
+      assert.equal(converter.processInput('1kg'),{value: 1, type: "kg"});
+      assert.equal(converter.processInput('1km'),{value: 1, type: "km"});
+      done();
+    }); 
+  });
 });
