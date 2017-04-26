3a4fc5685a20c60d2385a58fa95662c6cec7443c
Selhar <selhar@protonmail.com>
2017-04-26 02:18:24 -0300

    CRUD

--- infosec-and-qa/personal-library/controllers/create.js
+++ infosec-and-qa/personal-library/controllers/create.js
@@ -1,36 +1,32 @@
 const mongoose = require('mongoose');
-const PLACEHOLDERMODEL = require('PLACEHOLDER');
+const BookModel = require('../models/book');
 const waterfall = require("async/waterfall");
 const IssueModel = require('../models/issue');
 
 exports.create = (request, response) => {
     waterfall([ 
-        function isPLACEHOLDERInDB(callback){
-            PLACEHOLDERMODEL.findOne( {PARAMETER: PLACEHOLDERDATA}, (error, project) =>{
+        function isBookInDB(callback){
+            BookModel.findOne( {title: request.body.title}, (error, book) =>{
                 if(error){
                     return callback(error);
-                }else if(project){
-                    return callback(null, project);
+                }else if(book){
+                    return done("A book with this name already exists");
                 }else{
-                    return done("there are no PLACEHOLDERS with the parameters provided "+request.params.project);
+                    return callback(null);
                 }
             });
-      }, function savePLACEHOLDER(isPLACEHOLDERInDB, PLACEHOLDER, callback){
+      }, function saveBook(callback){
 
-          if(isPLACEHOLDERInDB){
-            return callback(null, PLACEHOLDER);
-          }else{
-            let new_PLACEHOLDER = new PLACEHOLDERMODEL({
-                FIELD: request.params.PLACEHOLDER
+            let new_book = new BookModel({
+                title: request.body.title
             });
-            new_PLACEHOLDER.save((error) => {
+            new_book.save((error) => {
                 if(error){
                     return callback(error);
                 }
-                return callback(null, new_PLACEHOLDER);
+                return callback(null, new_book);
             });
           }
-      }
     ], done);
 
     function done(error, result) {
@@ -38,6 +34,6 @@ exports.create = (request, response) => {
             console.log('\nError during fetch process: '+error+'\n');
             return response.send(error);
         }
-        return response.json(result);
+        return response.json({title: result.title, _id: result._id});
     }
 }
