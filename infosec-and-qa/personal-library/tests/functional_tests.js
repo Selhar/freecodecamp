00b2214cea77b6101506f1fe400660cd6a1e5c28
Selhar <selhar@protonmail.com>
2017-04-26 02:38:36 -0300

    CRUD

--- infosec-and-qa/personal-library/tests/functional_tests.js
+++ infosec-and-qa/personal-library/tests/functional_tests.js
@@ -4,12 +4,22 @@ const server = require('../server');
 const assert = chai.assert;
 
 chai.use(chai_http);
+let id = '';
 
 suite('Functional testing', () => {
-    suite('TEMPORARY TEST LABEL', () => {
-        test('TEMPORARY TEST LABEL', (done) => {
-            
-            done();
+    suite('Create a new book', () => {
+        test('Create the book', (done) => {
+            chai.request(server).post('/api/books').send({title: 'The second foundation'}).end((request, response) => {
+                assert.deepEqual(response.body.title, 'The second foundation');
+                id = response.body._id;
+                done();
+            });
         });
-    })
+        test('Create a new comment', (done) => {
+            chai.request(server).post('/api/books/'+id).send({comment: 'The mule was right tho'}).end((request, response) => {
+                assert.deepEqual(response.body.comment, 'The mule was right tho');
+                done();
+            });         
+        });
+    });
 });
