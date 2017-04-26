4a1712af7ad4fc46a2131af1d50abbdd13170d51
Selhar <selhar@protonmail.com>
2017-04-26 00:51:52 -0300

    boilerplate

---
+++ infosec-and-qa/boilerplate/models/schema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const FOREIGN_KEY = require('./FOREIGN_KEY');
+
+const sample = new Schema({
+  SAMPLE: { type: String, required: true },
+  SAMPLE: { type: Date, default: Date.now },
+  SAMPLE: { type: Date},
+  SAMPLE: { type: Boolean },
+  SAMPLE: { type: Schema.Types.ObjectId, ref: 'FOREIGNKEY', required: true }
+});
+
+module.exports = mongoose.model('Issue', Issue)
+  
---
+++ infosec-and-qa/boilerplate/tests/unit_tests.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+suite('Functional testing', () => {
+    suite('TEMPORARY TEST LABEL', () => {
+        test('TEMPORARY TEST LABEL', (done) => {
+            
+            done();
+        });
+    })
+});
