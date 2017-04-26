05db3bd8e8d903e1575ff46d9038dff9cbad4660
Selhar <selhar@protonmail.com>
2017-04-26 00:40:53 -0300

    personal library boilerplate

--- infosec-and-qa/boilerplate/server.js
+++ infosec-and-qa/boilerplate/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const helmet = require('helmet');
+const body_parser = require('body-parser');
+const mongoose = require('mongoose');
 const server = express();
 const root = process.cwd();
+
+mongoose.connect('mongodb://localhost:/27017/                                      TEMPORARY_NAME');
+
+server.use(helmet());
 server.use('/public', express.static(root + '/public'));
+server.use(body_parser.json());
+server.use(body_parser.urlencoded( {extended: true} ));
 
 server.get('/', (request, response) => {
     response.render(root + '/views/index.ejs');
 });
+
+server.post('', TO.do);
+server.put('', TO.do);
+server.get('', TO.do);
+server.delete('', TO.do);
 
 server.get('*', (request, response) => {
     response.send('<p>Bad, bad user. No donuts for you.</p>');
@@ -15,4 +29,4 @@ server.listen(process.env.PORT || 3000, () => {
     console.log("Listening");
 });
 
-module.exports = server;
+module.exports = server; // for testing
