f7bc0a5fd46aa761e4f718f79108e94910082a04
Selhar <selhar@protonmail.com>
2017-04-26 00:36:53 -0300

    personal library boilerplate

---
+++ infosec-and-qa/personal-library/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const helmet = require('helmet');
+const body_parser = require('body-parser');
+const mongoose = require('mongoose');
+const server = express();
+const root = process.cwd();
+
+mongoose.connect('mongodb://localhost:/27017/personallibrary');
+
+server.use(helmet());
+server.use('/public', express.static(root + '/public'));
+server.use(body_parser.json());
+server.use(body_parser.urlencoded( {extended: true} ));
+
+server.get('/', (request, response) => {
+    response.render(root + '/views/index.ejs');
+});
+
+server.post('', TO.do);
+server.put('', TO.do);
+server.get('', TO.do);
+server.delete('', TO.do);
+
+server.get('*', (request, response) => {
+    response.send('<p>Bad, bad user. No donuts for you.</p>');
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Listening");
+});
+
+module.exports = server; // for testing
