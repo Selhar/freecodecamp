49a52db8c0ab88ee3bf4fc051893462401378a54
Selhar <selhar@protonmail.com>
2017-04-29 02:20:57 -0300

    returning api call normally

--- infosec-and-qa/stock-price-checker/controllers/fetch.js
+++ infosec-and-qa/stock-price-checker/controllers/fetch.js
@@ -3,7 +3,7 @@ const req = require('request');
 exports.fetch = (request, response) => {
     const stock = request.query.stock;
     const isLiked = request.query.like;
-    const API = 'https://finance.google.com/finance/info?q=NASDAQ%3aGOOG';
+    const API = 'https://finance.google.com/finance/info?q=NASDAQ%3a'+stock;
 
     req.get({url: API,json: true,headers: {'User-Agent': 'request'}}, (error, res, data) => {
         if (error) {
