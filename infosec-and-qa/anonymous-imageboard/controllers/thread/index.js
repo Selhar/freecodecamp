46c014a5a3ea6738cc6e5cabbac35ee0198969ed
Selhar <selhar@protonmail.com>
2017-05-01 03:07:49 -0300

    data modeling tests

---
+++ infosec-and-qa/anonymous-imageboard/controllers/thread/index.js
@@ -0,0 +1,31 @@
+const create = require('./create');
+const fetch = require('./fetch');
+const remove = require('./remove');
+const update = require('./update');
+
+exports.create = create.create;
+exports.fetch = fetch.fetch;
+exports.update = update.update;
+exports.remove = remove.remove;
+
+
+const Thread = require('../../models/Thread');
+
+exports.teste = (request, response) => {
+    let thread = new Thread({
+        title: "teste",
+        password: "boga",
+        replies: [
+            {
+                text: "teste1"
+            },
+            {
+                text: "teste2"
+            }
+        ]
+    });
+
+    thread.save((error, data) => {
+        console.log(data);
+    })
+}
