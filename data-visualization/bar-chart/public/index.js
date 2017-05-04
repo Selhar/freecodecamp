ac0d6ee18fc6325aeee3bf0e19a7e29aed0a34c1
Selhar <selhar@protonmail.com>
2017-05-04 12:20:11 -0300

    grokking d3

--- data-visualization/bar-chart/public/index.js
+++ data-visualization/bar-chart/public/index.js
@@ -1,3 +1,15 @@
+/* 
+    Configuration variables
+*/
+let fallback_data = {};
+
+d3.json('./data_fallback.json', (error, data) => {
+    if(error){
+        console.log(error);
+    }
+    fallback_data = data;
+});
+
 const margin = {
     top: 50, 
     right: 25, 
@@ -10,11 +22,27 @@ const dimensions = {
     height: 500 - (margin.top + margin.bottom)
 };
 
-let xAxis = d3.scaleBand().range([0, dimensions.width]).padding(0.15);
+let xAxis = d3.scaleLinear().range([0, dimensions.width]).padding(0.15);
 let yAxis = d3.scaleLinear().range([dimensions.height, 0]);
 
-let svg = d3.select('body').append('svg')
+let container = d3.select('body').append('svg')
         .attr('width', dimensions.width + margin.left + margin.right)
         .attr('height', dimensions.height + margin.top + margin.bottom)
     .append('g')
         .attr('transform','translate('+margin.left+','+margin.top+')');
+
+/* 
+    Configuration variables
+*/
+
+
+d3.json('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', (error, data) => {
+    if(error){
+        console.log(error+"\n\nAn error ocurred with the remote API, using local fallback data from march 2017");
+        data = fallback_data;
+    }
+
+    xAxis.domain([data.from_date, data.to_date]);
+    yAxis.domain([]);
+});
+        
