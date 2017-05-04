b6c34a40fa041ea8a799aeaf999bb32bfef59192
Selhar <selhar@protonmail.com>
2017-05-04 02:47:11 -0300

    grokking d3

--- data-visualization/bar-chart/public/index.js
+++ data-visualization/bar-chart/public/index.js
@@ -1,25 +1,18 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
+var margin = {top: 40, right: 20, bottom: 40, left: 40},
     width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 550 - margin.top - margin.bottom;
 
-// set the ranges
-var x = d3.scaleBand()
-          .range([0, width])
-          .padding(0.1);
-var y = d3.scaleLinear()
-          .range([height, 0]);
+
+var x = d3.scaleBand().range([0, width]).padding(0.15);
+var y = d3.scaleLinear().range([height, 0]);
           
-// append the svg object to the body of the page
-// append a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
 var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                .attr("transform", 
+                     "translate(" + margin.left + "," + margin.top + ")");
 
-// get the data
 d3.csv("data.csv", function(error, data) {
   if (error) throw error;
 
