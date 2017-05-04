916c9475150059ff34740984fe9f9c0ca2b6ef22
Selhar <selhar@protonmail.com>
2017-05-04 02:32:17 -0300

    grokking d3

--- data-visualization/bar-chart/public/index.js
+++ data-visualization/bar-chart/public/index.js
@@ -1,42 +1,54 @@
-let width = 700;
-let height = 500;
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-let x = d3.scaleBand().range([0, width]).padding(0.1);
-let y = d3.scaleBand().range([height, 0]);
+// set the ranges
+var x = d3.scaleBand()
+          .range([0, width])
+          .padding(0.1);
+var y = d3.scaleLinear()
+          .range([height, 0]);
+          
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
 
-let svg = d3.select('graph')
-                .append('svg')
-                    .attr('width', width)
-                    .attr('height', height)
-                .append('g');
-d3.csv('data.csv', (error, data) => {
-    if(error)
-        throw error;
-    
-    data.forEach((d) => {
-        d.sales += d.sales;
-    });
+// get the data
+d3.csv("data.csv", function(error, data) {
+  if (error) throw error;
 
-    x.domain(data.map((d) => {return d.salesperson; }));
-    y.domain([0, d3.max(data, (d) => {return d.sales; })]);
+  // format the data
+  data.forEach(function(d) {
+    d.sales = +d.sales;
+  });
 
-    svg.selectAll('.bar')
-            .data(data)
-        .enter().append('rect')
-            .attr('class','bar')
-            .attr('x',(d) => {return x(d.salesperson);})
-            .attr('width', x.bandwidth)
-            .attr('y',(d) => {return y(d.sales);})
-            .attr('height', (d) => { return height - y(d.sales); });
-    
-    svg.append('g')
-        .attr('transform', 'translate(0,'+height+')')
-        .call(d3.axisBottom(x));
-    
-    svg.append('g')
-        .call(d3.axisLeft(y));
+  // Scale the range of the data in the domains
+  x.domain(data.map(function(d) { return d.salesperson; }));
+  y.domain([0, d3.max(data, function(d) { return d.sales; })]);
+
+  // append the rectangles for the bar chart
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.salesperson); })
+      .attr("width", x.bandwidth())
+      .attr("y", function(d) { return y(d.sales); })
+      .attr("height", function(d) { return height - y(d.sales); });
+
+  // add the x Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // add the y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
 });
-
-
-                    
