b8d6d86aafbfbc0d9f60333e3fe3c51985b322f7
Selhar <selhar@protonmail.com>
2017-05-09 13:26:31 -0300

    wireframe

---
+++ frontend-frameworks/calculator/src/components/button.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class Button extends Component{
+    render(){
+        return(
+        <div className="button">
+
+        </div>
+    )}
+}
+
+// export default connect(
+//    state => ({
+//     }),
+//    dispatch => ({
+//     })
+// )(Clock);
--- frontend-frameworks/calculator/src/components/indexComponent.js
+++ frontend-frameworks/calculator/src/components/indexComponent.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import Display from './display';
+import Button from './button';
 
 export default () => (
     <div className="board">     
         <Display />   
+        <Button />  
+        <Button />  
+        <Button />  
+        <Button />
+        <Button />
+        <Button />        
+        <Button />  
+        <Button />  
+        <Button />  
+        <Button />
+        <Button />
+        <Button />
+        <Button />  
+        <Button />
+        <Button />
+        <Button />
+        <Button />  
+        <Button />
+        <Button />
+        <Button />
     </div>
 )
