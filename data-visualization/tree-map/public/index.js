376f6512b09409374ef98eafeac781034b4edebc
Selhar <selhar@protonmail.com>
2017-05-09 02:33:17 -0300

    legend

---
+++ data-visualization/tree-map/public/index.js
@@ -0,0 +1,151 @@
+/* 
+    Configuration variables
+*/
+const container_div = document.querySelector(".container");
+const container_styles = window.getComputedStyle(container_div);
+
+const container_dimensions = {
+    width: container_styles.getPropertyValue("width").slice(0,-2),
+    height: container_styles.getPropertyValue("height").slice(0,-2)
+}
+const padding_divisor = 10;
+const svg = {
+    width: container_dimensions.width,
+    height: container_dimensions.height,
+    padding: {
+        height: container_dimensions.height / padding_divisor,
+        width: container_dimensions.width / padding_divisor,
+        //total padding divided by 2 = padding from each individual side
+        width_side: (container_dimensions.width / padding_divisor) / 2,
+        height_side: (container_dimensions.height / padding_divisor) / 2
+    }
+}
+
+//i need an early reference to container so i can redraw the screen
+let legend_container;
+let container;
+
+let data_storage = {
+    kickstarter : '',
+    movie: '',
+    video: ''
+}
+
+let fader = (color) => d3.interpolateRgb(color, "#fff")(0.2);
+let color = d3.scaleOrdinal(d3.schemeCategory20.map(fader));
+
+let tooltip_block = d3.select("body")
+                    .append("div")
+                    .attr("class", "tooltip")
+                    .style("opacity", 0);
+
+/* data fetching and formatting */
+d3.queue()
+    .defer(d3.json, 'https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/kickstarter-funding-data.json')
+    .defer(d3.json, 'https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json')
+    .defer(d3.json, 'https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json')
+    .await(render_data);
+
+function render_data (error, kickstarter, movie, video, user_input) {
+    if(error){
+        console.log(error);
+        return alert('An error ocurred with the remote API, using local fallback data from march 2017');
+    }
+    if(data_storage.kickstarter === ''){
+        data_storage.kickstarter = kickstarter;
+        data_storage.movie = movie;
+        data_storage.video = video;
+        render(data_storage.kickstarter)
+    }else{
+        container.remove();
+        legend_container.remove();
+        render(data_storage[user_input]);
+    }
+    
+}
+/* Data rendering */
+function render (data) {    
+    container = d3.select('.graph').append('svg')
+                    .attr('width', svg.width)
+                    .attr('height', svg.height );
+    
+    let root = d3.hierarchy(data)
+                .sum((d) => d.value)
+                .sort(function(a, b) { return b.height - a.height 
+                                            || b.value - a.value; });
+
+    let treemap = d3.treemap()
+                .size([svg.width, svg.height])
+                .paddingInner(1);
+
+    treemap(root);
+
+    let cell = container.selectAll('g')
+                .data(root.leaves())
+                .enter().append('g')
+                .attr("transform", (d) => { 
+                    return "translate(" + d.x0 + "," + d.y0 + ")"; 
+                });
+
+     let tile = cell.append("rect")
+                .attr("width", (d) => d.x1 - d.x0)
+                .attr("height", (d) => d.y1 - d.y0)
+                .attr('fill', (d) => color(d.data.category))
+                .on("mousemove", (d) => {  
+                    tooltip_block.style("opacity", 0.9); 
+                    tooltip_block.html(
+                        'Name: ' + d.data.name + 
+                        '</br>Category: ' + d.data.category + 
+                        '</br>Value: ' + d.data.value
+                    )
+                    .style("left", (d3.event.pageX) + "px")		
+                    .style("top", (d3.event.pageY - 28) + "px")
+                })    
+                .on("mouseout", function(d) { 
+                    tooltip_block.transition()
+                        .duration(150).style('opacity', 0);
+                });
+            
+    const body_selector = document.querySelector("body");
+    const body_styles = window.getComputedStyle(body_selector);
+    const font_size = body_styles.getPropertyValue("font-size").slice(0,-2);
+    const font_padding = 10;
+    const legend_color_width = 15;
+    cell.append("text")
+        .selectAll("tspan")
+        .data((d) => d.data.name.split(/(?=[A-Z][^A-Z])/g))
+        .enter().append("tspan")
+        .attr("x", font_padding)
+        .attr("y", (d, i) => font_size * i + 10)
+        .text(function(d) { return d; });
+
+    var categories = root.leaves().map(function(nodes){
+    return nodes.data.category;
+    });
+    categories = categories.filter(function(category, index, self){
+    return self.indexOf(category)===index;    
+    })
+
+    legend_container = d3.select('.legend').append('svg')
+                    .attr('width', svg.width /4)
+                    .attr('height', svg.height );
+
+    let legend = legend_container.append('g')
+        .selectAll('g')
+        .data(categories)
+        .enter().append('g');
+    
+    legend.append('rect')
+                .attr('width', legend_color_width)
+                .attr('height', legend_color_width)
+                .attr('x', 20)
+                .attr('y', (d, i) => position = (legend_color_width + 15) * i)
+                .attr('fill', (d) => color(d));
+    
+    legend.append('text')
+        .attr('transform', 'translate(0, 10)')  
+        .attr('x', 40)
+        .attr('y', (d, i) => (legend_color_width + 15) * i)
+        .text((d => d))
+    
+}
