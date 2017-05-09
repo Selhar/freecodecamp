40b0ecb44b6e55ac2c3125abedf93783935a0985
Selhar <selhar@protonmail.com>
2017-05-09 18:17:36 -0300

    change display state

--- frontend-frameworks/calculator/src/actions/indexAction.js
+++ frontend-frameworks/calculator/src/actions/indexAction.js
@@ -2,9 +2,9 @@ export const actionTypes = {
     change_display_value: "CHANGE_DISPLAY"
 }
 
-export const change_display_value = (value) => {
+export const change_display_value = (display_value) => {
     return{
         type: actionTypes.change_display_value,
-        value
+        display_value
     }
 }
--- frontend-frameworks/calculator/src/components/button.js
+++ frontend-frameworks/calculator/src/components/button.js
@@ -1,18 +1,26 @@
 import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {change_display_value} from '../actions/indexAction';
 
-export default class Button extends Component{
+ class Button extends Component{
     render(){
         const {label} = this.props;
+        const change_display = () => {
+            this.props.change_display_value(label);
+        }
+
         return(
-        <div className="button">
+        <div className="button" onClick={change_display}>
             {label}
         </div>
     )}
 }
 
-// export default connect(
-//    state => ({
-//     }),
-//    dispatch => ({
-//     })
-// )(Clock);
+export default connect(
+   state => ({
+   }),
+   dispatch => ({
+     change_display_value: bindActionCreators(change_display_value, dispatch)
+   })
+)(Button);
