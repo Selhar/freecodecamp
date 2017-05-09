e277fbdeaf4ebfdae06efb58d6bf9235aa203f96
Selhar <selhar@protonmail.com>
2017-05-09 15:12:07 -0300

    wireframe

--- frontend-frameworks/calculator/src/components/button.js
+++ frontend-frameworks/calculator/src/components/button.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 
 export default class Button extends Component{
     render(){
+        const {label} = this.props;
         return(
         <div className="button">
-
+            {label}
         </div>
     )}
 }
--- frontend-frameworks/calculator/src/components/indexComponent.js
+++ frontend-frameworks/calculator/src/components/indexComponent.js
@@ -5,25 +5,24 @@ import Button from './button';
 export default () => (
     <div className="board">     
         <Display />   
-        <Button />  
-        <Button />  
-        <Button />  
-        <Button />
-        <Button />
-        <Button />        
-        <Button />  
-        <Button />  
-        <Button />  
-        <Button />
-        <Button />
-        <Button />
-        <Button />  
-        <Button />
-        <Button />
-        <Button />
-        <Button />  
-        <Button />
-        <Button />
-        <Button />
+        <Button label="C"/>  
+        <Button label="±"/>  
+        <Button label="%"/>  
+        <Button label="÷"/>
+        <Button label="7"/>
+        <Button label="8"/>        
+        <Button label="9"/>  
+        <Button label="X"/>  
+        <Button label="4"/>  
+        <Button label="5"/>
+        <Button label="6"/>
+        <Button label="−"/>
+        <Button label="1"/>  
+        <Button label="2"/>
+        <Button label="3"/>
+        <Button label="+"/>
+        <Button label="0"/>  
+        <Button label="●"/>
+        <Button label="="/>
     </div>
 )
