b270f79006d66c22de6b0e535fc76e533f763087
Selhar <selhar@protonmail.com>
2017-05-11 16:51:53 -0300

    project showcase

--- algorithms-and-datastructures/intermediate-algorithm-scripting/sum-all-numbers-in-range.js
+++ algorithms-and-datastructures/intermediate-algorithm-scripting/sum-all-numbers-in-range.js
@@ -1,9 +1,10 @@
 function sumAll(arr) {
   arr.sort();
   let sum = 0;
-  for(let i = arr[0]; i<= arr[-1]; i++){
-    sum += arr[i];
+  for(let i = arr[0]; i<= arr[arr.length-1]; i++){
+    sum += i;
   }
+  return sum;
 }
 
-console.log(sumAll([1, 4]));
+sumAll([1, 4]);
