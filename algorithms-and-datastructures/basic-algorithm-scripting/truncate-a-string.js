e2d33236d01efd814b35b920cd2e68a101cbab1a
Selhar <selhar@protonmail.com>
2017-05-11 15:37:30 -0300

    basic algorithm scripting

--- algorithms-and-datastructures/basic-algorithm-scripting/truncate-a-string.js
+++ algorithms-and-datastructures/basic-algorithm-scripting/truncate-a-string.js
@@ -1,7 +1,5 @@
-
 function truncateString(str, num) {
-    console.log(str = str.substring(0, num).append('...'));
-  return str.length < num ? str = str.substring(0, num).append('...') : str;
+  return str.length > num ? str.substring(0, num) + '...' : str;
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 2);
+truncateString("Absolutely Longer", 2);
