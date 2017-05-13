2fea9feae4e3b3347de9d9209a1d6d45d867013c
Selhar <selhar@protonmail.com>
2017-05-13 06:55:20 -0300

    front end for issue tracker

--- infosec-and-qa/issue-tracker/controllers/create.js
+++ infosec-and-qa/issue-tracker/controllers/create.js
@@ -63,7 +63,7 @@ exports.create = (request, response) => {
 
         let shouldRenderEjs = request.params.project == 'noid' ? true : false;
         if(shouldRenderEjs){
-            return response.render('../views/issue.ejs', {data: result});
+            return response.render('../views/issue.ejs', {data: [result], home_url:'http://'+request.headers.host});
         }
         return response.json(result);
     }
