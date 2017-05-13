4fa29bfd9d5c34ade0160ab8c790dfa7a64e0956
Selhar <selhar@protonmail.com>
2017-05-12 23:15:50 -0300

    front end for ib

--- infosec-and-qa/anonymous-imageboard/server.js
+++ infosec-and-qa/anonymous-imageboard/server.js
@@ -15,6 +15,7 @@ const api_root = '/';
 const thread_controller = require('./controllers/thread/index');
 const replies_controller = require('./controllers/replies/index');
 const ThreadModel = require('./models/Thread');
+const ObjectId = require('mongodb').ObjectID;
 
 mongoose.connect("mongodb://localhost:27017/anonymous-imageboard");
 
@@ -34,9 +35,11 @@ server.use(body_parser.urlencoded( {extended: true} ));
 
 /*
 The freecodecamp test suite require the server response to be on JSON
-I can't return JSON to render a page, thus 2 pages were created
+I can't return JSON to render a page, thus 2 server responses were created
+They're pretty much similar, but tests should never be done this way. (the official solution is also similar)
+
 This curriculum is still in beta so this might be an oversight
-either way, to test the project just uncomment beolw.
+either way, to test the project just uncomment below.
 
 Also, routes are slightly different because you can't DELETE through http forms
 
@@ -56,6 +59,8 @@ server.delete(api_root+':thread_id', replies_controller.remove);
 
 */
 
+
+//Get complete thread list
 server.get(api_root, (request, response) => {
     ThreadModel.find({})
                 .select('_id creation_date last_post text title replies')
@@ -69,6 +74,22 @@ server.get(api_root, (request, response) => {
     });
 });
 
+//Show a single thread
+server.get(api_root+':thread_id', (request, response) => {
+    ThreadModel.findById(request.params.thread_id)
+                .select('_id creation_date last_post text title replies')
+                .exec((error, threads) =>{
+        if(error){
+            return callback(error);
+        }else if(threads){
+            return callback(null, threads);
+        }else{
+            return callback("There isn't a thread with this ID.");
+        }
+    });
+});
+
+//Make a new thread
 server.post(api_root, (request, response) => {
     let url_user_came_from = request.protocol + '://' + request.get('Host');
     
@@ -86,23 +107,25 @@ server.post(api_root, (request, response) => {
     });
 });
 
+//Make a new comment
 server.post(api_root+':thread', (request, response) => {
     let url_user_came_from = request.protocol + '://' + request.get('Host');
-    
-    let new_thread = new ThreadModel({
-        title: request.body.title,
-        text: request.body.text,
-        password: request.body.password
-    });
-
-    new_thread.save((error) => {
-        if(error){
+    let id = new ObjectId();
+    ThreadModel.findByIdAndUpdate(request.body.thread_id, {
+        $push: {replies: {
+            text: request.body.text, 
+            password: request.body.password,
+            _id: id
+        }}
+    }, (error, data) => {
+        if(error)
             throw error;
-        }
-        response.redirect(url_user_came_from+'/'+new_thread._id);
-    });
+        else
+            response.redirect(url_user_came_from+'/'+id);
+    })
 });
 
+//Delete a thread
 server.get(api_root+'delete', (request, response) => {
     let url_user_came_from = request.protocol + '://' + request.get('Host');
     ThreadModel.findOneAndRemove({
@@ -115,10 +138,6 @@ server.get(api_root+'delete', (request, response) => {
             response.redirect(url_user_came_from);
     });
 
-});
-
-server.get('/:id', (request, response) => {
-    response.render(root + '/views/thread.ejs');
 });
 
 server.get('*', (request, response) => {
