54d5aafc7f1fac1cc65b4de0527da7467304973c
Selhar <selhar@protonmail.com>
2017-07-25 23:36:00 -0300

    board state setup

--- vanilla/front-end/tictactoe/js/js.js
+++ vanilla/front-end/tictactoe/js/js.js
@@ -1,11 +1,24 @@
-ready(() => {
-});
+let board_state = [[0,0,0],
+                    [0,0,0],
+                    [0,0,0]];
+const O = 0;
+const X = 1;
 
-//Function from http://youmightnotneedjquery.com/
-function ready(fn) {
-    if (document.readyState != 'loading') {
-        fn();
-    } else {
-        document.addEventListener('DOMContentLoaded', fn);
+function process_move(id) {
+    let position_1;
+    let position_2;
+
+    if(id <= 3){
+        position_1 = 0;
+        position_2 = id-1;
+    }else if(id <=6){
+        position_1 = 1;
+        position_2 = id % 3;
+    }else{
+        position_1 = 2;
+        position_2 = id % 3;
     }
+
+    board_state[position_1][position_2] = X;
+    console.log(board_state);
 }
