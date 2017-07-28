e81eda5411d41ee96f69069a272250200980a686
Selhar <selhar@protonmail.com>
2017-07-28 03:07:08 -0300

    basic enemy functionality

--- vanilla/front-end/tictactoe/js/js.js
+++ vanilla/front-end/tictactoe/js/js.js
@@ -1,7 +1,8 @@
 const O = 1;
 const X = 2;
 let player;
-let move_count = 0;
+let machine;
+let turn = 0;
 
 let board_state = [[0,0,0],
                    [0,0,0],
@@ -9,123 +10,144 @@ let board_state = [[0,0,0],
 const original_state = board_state;
 
 function manage_game(id){
-
-    fill_node(id);
-    process_move(id);
-    move_count++;
+  process_move(id);
+  enemy_move();
+  turn++;
     
-    if(move_count >= 3 && is_won()){
-        console.log(is_won());
-    }else{
-        console.log(board_state);
+  if(turn >= 3 && is_won()){
+    console.log(is_won());
+  }else{
+    console.log(board_state);
+  }
+}
+
+function enemy_move(){
+ let isRowValid;
+ let validPosition;
+  for(let i = 0; i<board_state.length; i++){
+    isRowValid = true;
+    validPosition = [];
+    for(let j = 0; j<board_state.length; j++){
+      if(board_state[i][j] == player){
+        isRowValid = false;
+      }else if(board_state[i][j] == 0){
+        validPosition = [i,j];
+      }
     }
+    if(isRowValid){
+      let valid_id = (validPosition[0] * 3) + validPosition[1]; 
+      fill_node(valid_id, "O");
+      break;
+    }
+  }
 }
 
 function process_move(id) {
-    let position_1;
-    let position_2;
+  let position_1;
+  let position_2;
 
-    if(id <= 3){
-        position_1 = 0;
-        position_2 = id-1;
-    }else if(id <= 6){
-        position_1 = 1;
-        position_2 = Math.floor((id-1) % 3);
-    }else{
-        position_1 = 2;
-        position_2 = Math.floor((id-1) % 3);
-    }
-    board_state[position_1][position_2] = X;
+  if(id <= 3){
+      position_1 = 0;
+      position_2 = id-1;
+  }else if(id <= 6){
+      position_1 = 1;
+      position_2 = Math.floor((id-1) % 3);
+  }else{
+      position_1 = 2;
+      position_2 = Math.floor((id-1) % 3);
+  }
+  board_state[position_1][position_2] = player;
+  fill_node(id, "X");
 }
 
-function fill_node(id){
-    let symbol = player == 1 ? "O" : "X";
-    let node = document.getElementById(id);
+function fill_node(id, symbol){
+  let node = document.getElementById(id);
 
-    node.innerHTML = symbol;
+  node.innerHTML = symbol;
 }
 
 function is_won(){
-    //The sum of a winning move is always 3 or 6, according to X and O's value.
-    //The sum of a losing move is always modulus > 0
+  //The sum of a winning move is always 3 or 6, according to X and O's value.
+  //The sum of a losing move is always modulus > 0
 
-    let position_1 = 0;
-    let position_2 = 0;
-    let sum = 0;
-    let i = 0;
-    let j = 0;
-    let game_over = false;
+  let position_1 = 0;
+  let position_2 = 0;
+  let sum = 0;
+  let i = 0;
+  let j = 0;
+  let game_over = false;
 
-    //Horizontal verification
-    for(i = 0; i<board_state.length; i++){
-        sum = 0;
-        for(j = 0; j<board_state.length; j++){
-            sum += board_state[i][j];
-        }
-        if(sum > 0 && sum % 3 == 0){
-            game_over = true;
-            break;
-        }
-    }
+  //Horizontal verification
+  for(i = 0; i<board_state.length; i++){
+      sum = 0;
+      for(j = 0; j<board_state.length; j++){
+          sum += board_state[i][j];
+      }
+      if(sum > 0 && sum % 3 == 0){
+          game_over = true;
+          break;
+      }
+  }
 
-    if(!game_over){
-    //Vertical verification
-        for(i = 0; i<board_state.length; i++){
-            sum = 0;
-            for(j = 0; j<board_state.length; j++){
-                sum += board_state[j][i];
-            }
-            if(sum > 0 && sum % 3 == 0){
-                game_over = true;
-                break;
-            }
-        }
-    }
+  if(!game_over){
+  //Vertical verification
+      for(i = 0; i<board_state.length; i++){
+          sum = 0;
+          for(j = 0; j<board_state.length; j++){
+              sum += board_state[j][i];
+          }
+          if(sum > 0 && sum % 3 == 0){
+              game_over = true;
+              break;
+          }
+      }
+  }
 
-    if(!game_over){
-    //Right side diagonal verification
-        i = 0;
-        j = 0;
-        sum = 0;
-        
-        while(i <= 2){
-            sum += board_state[i][j];
-            i++;
-            j++;
-        }
+  if(!game_over){
+  //Right side diagonal verification
+      i = 0;
+      j = 0;
+      sum = 0;
+      
+      while(i <= 2){
+          sum += board_state[i][j];
+          i++;
+          j++;
+      }
 
-        if(sum > 0 && sum % 3 == 0){
-            game_over = true;
-        }
-    }
+      if(sum > 0 && sum % 3 == 0){
+          game_over = true;
+      }
+  }
 
-    if(!game_over){
-    //Left side diagonal verification
-        i = 0;
-        j = 2;
-        sum = 0;
-            
-        while(i <= 2){
-            sum += board_state[i][j];
-            i++;
-            j--;
-        }
-        if(sum > 0 && sum % 3 == 0){
-            game_over = true;
-        }
+  if(!game_over){
+  //Left side diagonal verification
+      i = 0;
+      j = 2;
+      sum = 0;
+          
+      while(i <= 2){
+          sum += board_state[i][j];
+          i++;
+          j--;
+      }
+      if(sum > 0 && sum % 3 == 0){
+          game_over = true;
+      }
 
-    }
-    return game_over ? sum : false;
+  }
+  return game_over ? sum : false;
 }
 
 function choose_symbol(choice){
-    player = choice == "X" ? 2 : 1;
+  player = choice == "X" ? 2 : 1;
+  machine = choice == "X" ? 1 : 2;
 
-    let hidden = document.getElementsByClassName("hidden");
-    let options = document.getElementsByClassName("options");
-    options[0].style.display = "none";
+  let hidden = document.getElementsByClassName("hidden");
+  let options = document.getElementsByClassName("options");
+  options[0].style.display = "none";
 
-    for(i = 0; i < hidden.length; i++){
-        hidden[i].style.display = "block";
-    }
+  for(i = 0; i < hidden.length; i++){
+      hidden[i].style.display = "block";
+  }
 }
