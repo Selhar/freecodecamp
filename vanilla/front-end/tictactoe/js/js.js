0d27db3de0b198f49f0a1160bd9eaa63fed003d4
Selhar <selhar@protonmail.com>
2017-07-28 21:41:45 -0300

    refactoring

--- vanilla/front-end/tictactoe/js/js.js
+++ vanilla/front-end/tictactoe/js/js.js
@@ -1,170 +1,66 @@
-const O = 1;
-const X = 2;
-let player;
-let machine;
-let turn = 0;
+let GAME_STATE = [[0,0,0],
+                  [0,0,0],
+                  [0,0,0]];
 
-let board_state = [[0,0,0],
-                   [0,0,0],
-                   [0,0,0]];
+let MACHINE = {symbol: "", value: 2};
+let PLAYER = {symbol: "", value: 1};
 
+// assigns the choosen symbol to the human player
+// the other symbol automatically goes to the machine
+// after assignment, the DOM is updated with the game board
+function assign_symbol(id){
+  PLAYER.symbol = id;
+  MACHINE.symbol = id = "X" ? "X" : "O";
+
+  let game_elements = document.getElementsByClassName("hidden");
+  let assign_screen = document.getElementsByClassName("options");
+  assign_screen[0].style.display = "none";
+
+  for(i = 0; i < game_elements.length; i++){
+      game_elements[i].style.display = "block";
+  }
+}
+
+// Receives user input, processes, then makes the machine's move
 function manage_game(id){
-  process_move(id, "X");
-  enemy_move();
-  turn++;
+  game_action(id, PLAYER);
+  //enemy_movement();
+}
+
+// based on tile ID, updates the internal board
+// and draws the user's choice on the DOM
+function game_action(id, user) {
+  let board_coordinate = id_to_coordinate(id);
+  GAME_STATE = GAME_STATE[board_coordinate[0]][board_coordinate[1]] = user.symbol;
+  draw_node_to_DOM();
+  
+  // receives a tile ID and returns it's coordinate equivalent
+  function id_to_coordinate() {
+    let offset_id = id-1;
+    let row = Math.floor(offset_id / 3);
+    let column = offset_id % 3;
+
+    return [row, column];
+  }
+
+  // Uses coordinates to update the DOM representation of GAME_STATE
+  function draw_node_to_DOM() {
+    let element = document.getElementById(id);
+    element.innerHTML = user.symbol;
+  }
+}
+
+function enemy_move() {
+  
+  // Looks through the board for a valid tile and returns it's ID
+  // "valid tile" means that the slot is empty and it's row or column 
+  // has not yet been used by the opponent.
+  function analyse_valid_tile() {
     
-  if(turn >= 3 && is_won()){
-  //  console.log(is_won());
+    return valid_tile_ID;
   }
 }
 
-function enemy_move(){
-  let isRowValid = true;
-  let validPosition = [];
-
-  for(let i = 0; i<board_state.length; i++){
-    for(let j = 0; j<board_state.length; j++){
-      if(board_state[i][j] == player){
-        isRowValid = false;
-        break;
-      }else if(board_state[i][j] == 0){
-        validPosition = [i,j];
-      }
-    }
-    if(isRowValid){
-      draw(validPosition);
-      break;
-    }
-  }
-  if(!isRowValid){
-    for(let i = 0; i<board_state.length; i++){
-      for(let j = 0; j<board_state.length; j++){
-        if(board_state[j][i] == player){
-          isRowValid = false;
-          break;
-        }else if(board_state[j][i] == 0){
-          validPosition = [j,i];
-        }
-      }
-      if(isRowValid){
-        draw(validPosition);
-        break;
-      }
-    }
-  }
-
-  function draw(valid_position) {
-      let valid_id = (valid_position[0] * 3) + valid_position[1] +1; 
-      process_move(valid_id, "O");
-  }
-}
-
-function process_move(id, symbol) {
-  let position_1;
-  let position_2;
-
-  if(id <= 3){
-      position_1 = 0;
-      position_2 = id-1;
-  }else if(id <= 6){
-      position_1 = 1;
-      position_2 = Math.floor((id-1) % 3);
-  }else{
-      position_1 = 2;
-      position_2 = Math.floor((id-1) % 3);
-  }
-  board_state[position_1][position_2] = symbol;
-  fill_node(id, symbol);
-}
-
-function fill_node(id, symbol){
-  let node = document.getElementById(id);
-
-  node.innerHTML = symbol;
-}
-
-function is_won(){
-  //The sum of a winning move is always 3 or 6, according to X and O's value.
-  //The sum of a losing move is always modulus > 0
-
-  let position_1 = 0;
-  let position_2 = 0;
-  let sum = 0;
-  let i = 0;
-  let j = 0;
-  let game_over = false;
-
-  //Horizontal verification
-  for(i = 0; i<board_state.length; i++){
-      sum = 0;
-      for(j = 0; j<board_state.length; j++){
-          sum += board_state[i][j];
-      }
-      if(sum > 0 && sum % 3 == 0){
-          game_over = true;
-          break;
-      }
-  }
-
-  if(!game_over){
-  //Vertical verification
-      for(i = 0; i<board_state.length; i++){
-          sum = 0;
-          for(j = 0; j<board_state.length; j++){
-              sum += board_state[j][i];
-          }
-          if(sum > 0 && sum % 3 == 0){
-              game_over = true;
-              break;
-          }
-      }
-  }
-
-  if(!game_over){
-  //Right side diagonal verification
-      i = 0;
-      j = 0;
-      sum = 0;
-      
-      while(i <= 2){
-          sum += board_state[i][j];
-          i++;
-          j++;
-      }
-
-      if(sum > 0 && sum % 3 == 0){
-          game_over = true;
-      }
-  }
-
-  if(!game_over){
-  //Left side diagonal verification
-      i = 0;
-      j = 2;
-      sum = 0;
-          
-      while(i <= 2){
-          sum += board_state[i][j];
-          i++;
-          j--;
-      }
-      if(sum > 0 && sum % 3 == 0){
-          game_over = true;
-      }
-
-  }
-  return game_over ? sum : false;
-}
-
-function choose_symbol(choice){
-  player = choice == "X" ? 2 : 1;
-  machine = choice == "X" ? 1 : 2;
-
-  let hidden = document.getElementsByClassName("hidden");
-  let options = document.getElementsByClassName("options");
-  options[0].style.display = "none";
-
-  for(i = 0; i < hidden.length; i++){
-      hidden[i].style.display = "block";
-  }
+function update_game_state(id){
+  
 }
