f020862fac1602c3f67419d40d534a00f6777736
Selhar <selhar@protonmail.com>
2017-07-30 23:58:53 -0300

    enemy move

--- vanilla/front-end/tictactoe/js/js.js
+++ vanilla/front-end/tictactoe/js/js.js
@@ -5,8 +5,8 @@ let GAME_STATE = [[0,0,0],
 let MACHINE = {symbol: "", value: 2};
 let PLAYER = {symbol: "", value: 1};
 
-// assigns the choosen symbol to the human player
-// the other symbol automatically goes to the machine
+// assigns the choosen symbol to the human player,
+// the other symbol automatically goes to the machine.
 // after assignment, the DOM transitions into the game.
 function assign_symbol(id){
   PLAYER.symbol = id;
@@ -24,7 +24,7 @@ function assign_symbol(id){
 // Receives user input, updates GAME_STATE and the DOM, then makes the machine's move
 function manage_game(id){
   game_action(id, PLAYER);
-  //enemy_movement();
+  enemy_movement();
 }
 
 // based on tile ID, updates the internal board
@@ -34,7 +34,7 @@ function game_action(id, user) {
   let is_tile_empty = GAME_STATE[board_coordinate[0]][board_coordinate[1]]
   
   if(is_tile_empty === 0){
-    GAME_STATE[board_coordinate[0]][board_coordinate[1]] = user.symbol;
+    GAME_STATE[board_coordinate[0]][board_coordinate[1]] = user.value;
     draw_node_to_DOM();  
   }
   
@@ -53,13 +53,34 @@ function game_action(id, user) {
   }
 }
 
-function enemy_move() {
-  
+function enemy_movement() {
+  analyse_valid_tile();
+
+  // turns a column/row coordinate into a valid tile ID
+
+  function coordinate_to_id() {
+    
+  }
   // Looks through the board for a valid tile and returns it's ID
   // "valid tile" means that the slot is empty and it's row or column 
   // has not yet been used by the opponent.
   function analyse_valid_tile() {
-    
+    let is_row_valid = true;
+    let valid_tile = false;
+    for(let i = 0; i < GAME_STATE.length; i++){
+      for(let j = 0; j < GAME_STATE.length; j++){
+        if(GAME_STATE[i][j] == PLAYER.value){
+          is_row_valid = false;
+          break;
+        }
+        if(GAME_STATE[i][j] == 0){
+          valid_tile = {column: i, row: j};
+        }
+      }
+      if(is_row_valid && valid_tile !== false){
+        break;
+      }
+    }
     return valid_tile_ID;
   }
 }
